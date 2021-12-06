822edb7c1fe8f5365a6a61e2e95c6f4950510b10
b_scully <b.scully@f5.com>
2021-12-06 00:52:11 -0700

    Adding day 6

---
+++ 6/day6.py
@@ -0,0 +1,44 @@
+from collections import Counter
+
+with open('input.txt') as f:
+    input = [int(x) for x in f.readline().strip().split(',')]
+
+def main():
+    print("The solution for part 1 is: {0}".format(part1Solution(input)))
+    print("The solution for part 2 is: {0}".format(part2Solution(input)))
+
+def part1Solution(input):
+    fishes = input.copy()
+    newFish = 0
+    for i in range(80):
+        for j,fish in enumerate(fishes):
+            fishes[j] -= 1
+            if fish == 0:
+                fishes[j] = 6
+                newFish += 1
+        for x in range(newFish):
+            fishes.append(8)
+            newFish = 0
+#        print("Calculating day %s" % (i))
+    solution=len(fishes)
+    return solution
+
+def part2Solution(input):
+    count = Counter(input)
+    print(count)
+    for i in range(256): 
+        tempCount = Counter()
+        for num in count:
+            if num == 0:
+                #print(count)
+                tempCount[6] += count[0]
+                tempCount[8] += count[0]
+                print(count)
+            else: 
+                tempCount[num-1] += count[num]
+        count = tempCount
+        solution = sum(tempCount.values())
+    return solution
+
+if __name__ == "__main__":
+    main()
