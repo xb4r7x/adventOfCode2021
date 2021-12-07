bdacfd080aa715a9c335c50926b17253c582a535
b_scully <b.scully@f5.com>
2021-12-07 01:20:23 -0700

    Adding 7

---
+++ 7/day7.py
@@ -0,0 +1,33 @@
+with open('input.txt') as f:
+    input = [int(x) for x in f.readline().strip().split(',')]
+
+def main():
+    print("The solution for part 1 is: {0}".format(part1Solution(input)))
+    print("The solution for part 2 is: {0}".format(part2Solution(input)))
+
+def part1Solution(input):
+    lowest = float('inf')
+    for pos in input:
+        cost = 0
+        for dest in input:
+            distance = abs(dest - pos)
+            cost += distance
+        if cost < lowest:
+            lowest = cost
+    return lowest
+
+def part2Solution(input):
+    lowest = float('inf')
+    for pos in input:
+        cost = 0
+        for dest in input:
+            distance = abs(dest - pos)
+            for i in range(distance+1):
+                cost += i
+        if cost < lowest:
+            lowest = cost
+    return lowest
+    return 1
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # adventOfCode2021
-![](https://img.shields.io/badge/day%20📅-6-blue)
-![](https://img.shields.io/badge/stars%20⭐-10-yellow)	
-![](https://img.shields.io/badge/days%20completed-5-red)
+![](https://img.shields.io/badge/day%20📅-7-blue)
+![](https://img.shields.io/badge/stars%20⭐-12-yellow)	
+![](https://img.shields.io/badge/days%20completed-6-red)
 
 
 # newDay.sh
